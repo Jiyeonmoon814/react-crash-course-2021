42d682fb3356e7950a3dd8ce26f10c2c5ac12b12
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-05-23 17:39:19 +0900

    click event onDelete fx

--- src/App.js
+++ src/App.js
@@ -24,10 +24,15 @@ import Tasks from './components/Tasks'
         reminder:true,
     }
 ])
+
+//Delete Task
+const deleteTask = (id) => {
+  console.log('delete',id)
+}
    return (
      <div className="container">
        <Header />
-       <Tasks tasks={tasks}/>
+       <Tasks tasks={tasks} onDelete={deleteTask}/>
      </div>
    )
  }
