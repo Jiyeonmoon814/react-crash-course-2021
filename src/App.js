b6f29fe12fef4bf41a4cb3ef34b182a4e19ab895
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-05-23 18:21:45 +0900

    submit fx and onAdd show&hide

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
  function App() {
+   const [showAddTask,setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState( [
     {
         id:1,
@@ -26,6 +27,13 @@ import AddTask from './components/AddTask'
     }
 ])
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*10000)+1
+  const newTask = {id, ...task}
+  setTasks([...tasks,newTask])
+}
+
 //Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task)=>task.id!==id))
@@ -38,8 +46,8 @@ const toggleReminder = (id) => {
 }
    return (
      <div className="container">
-       <Header />
-       <AddTask />
+       <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length>0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) 
         : ('No Tasks To show')}
      </div>
