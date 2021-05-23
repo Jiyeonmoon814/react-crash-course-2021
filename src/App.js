ac154f8267bea8b4a4614eb600253b03d4062a97
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-05-23 17:04:23 +0900

    move useState to Appjs to use them in other comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 // import React from 'react'
+import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
  function App() {
+  const [tasks, setTasks] = useState( [
+    {
+        id:1,
+        text : 'Morning meeting',
+        day : 'May 24th at 11:00',
+        reminder:true,
+    },
+    {
+        id:2,
+        text : 'Lunch',
+        day : 'May 24th at 13:00',
+        reminder:true,
+    },
+    {
+        id:3,
+        text : 'Work out',
+        day : 'May 24th at 20:30',
+        reminder:true,
+    }
+])
    return (
      <div className="container">
        <Header />
-       <Tasks />
+       <Tasks tasks={tasks}/>
      </div>
    )
  }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,4 @@
-import {useState} from 'react'
-
-export const Tasks = () => {
-    const [tasks, setTasks] = useState( [
-        {
-            id:1,
-            text : 'Morning meeting',
-            day : 'May 24th at 11:00',
-            reminder:true,
-        },
-        {
-            id:2,
-            text : 'Lunch',
-            day : 'May 24th at 13:00',
-            reminder:true,
-        },
-        {
-            id:3,
-            text : 'Work out',
-            day : 'May 24th at 20:30',
-            reminder:true,
-        }
-    ])
-    
+const Tasks = ({tasks}) => {    
     return (
         <>
             {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}
