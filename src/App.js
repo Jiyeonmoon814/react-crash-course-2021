368301be5feb33793263c1f8cd32f8710582406a
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-05-23 17:41:50 +0900

    tasks.length>0 no tasks to show

--- src/App.js
+++ src/App.js
@@ -27,12 +27,12 @@ import Tasks from './components/Tasks'
 
 //Delete Task
 const deleteTask = (id) => {
-  console.log('delete',id)
+  setTasks(tasks.filter((task)=>task.id!==id))
 }
    return (
      <div className="container">
        <Header />
-       <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : ('No Tasks To show')}
      </div>
    )
  }
