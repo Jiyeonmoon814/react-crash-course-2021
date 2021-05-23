e513d8dc52ffe72eb0a6675c5008f16ec1929967
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-05-23 16:57:46 +0900

    set state as a part of component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-    {
-        id:1,
-        text : 'Morning meeting',
-        day : 'May 24th at 11:00',
-        reminder:true,
-    },
-    {
-        id:2,
-        text : 'Lunch',
-        day : 'May 24th at 13:00',
-        reminder:true,
-    },
-    {
-        id:3,
-        text : 'Work out',
-        day : 'May 24th at 20:30',
-        reminder:true,
-    }
-]
+import {useState} from 'react'
 
 export const Tasks = () => {
+    const [tasks, setTasks] = useState( [
+        {
+            id:1,
+            text : 'Morning meeting',
+            day : 'May 24th at 11:00',
+            reminder:true,
+        },
+        {
+            id:2,
+            text : 'Lunch',
+            day : 'May 24th at 13:00',
+            reminder:true,
+        },
+        {
+            id:3,
+            text : 'Work out',
+            day : 'May 24th at 20:30',
+            reminder:true,
+        }
+    ])
+    
     return (
         <>
             {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}
