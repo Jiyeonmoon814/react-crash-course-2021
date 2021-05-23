681f7adadb364c28a7642b236d08ca271ed84f22
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-05-23 18:56:17 +0900

    toggle reminder on Server

--- src/App.js
+++ src/App.js
@@ -24,6 +24,16 @@ const fetchTasks = async () => {
  return data
 }
 
+//Fetch Tasks
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+ return data
+}
+
+
+
 //Add Task
 const addTask = async (task) => {
   const res = await fetch('http://localhost:5000/tasks',{
@@ -51,9 +61,22 @@ const deleteTask = async (id) => {
 }
 
 //Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = {...taskToToggle, reminder:!taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'PUT',
+    headers: {
+      'Content-type':'application/json'
+    },
+    body:JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
   //!task.reminder means set an opposite thing if it's true it would be false after doubleclicking
-  setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:!task.reminder} : task))
+  setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:data.reminder} : task))
 }
    return (
      <div className="container">
