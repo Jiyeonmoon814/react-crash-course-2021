5449a6a215b01ec4d00e86c3807b34da3fbbd372
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-10 11:56:29 +0900

    Add EditTask component and update task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,23 @@
-import {FaTimes} from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa';
+import { FaEdit } from 'react-icons/fa';
+import { useState } from 'react'
 
-export const Task = ({task,onDelete,onToggle}) => {
+import EditTask from './EditTask';
+
+const Task = ({task , onDelete, onToggle}) => {
+    const [showEditTask, setShowEditTask] = useState(false)
     return (
-        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>
-            onToggle(task.id)}>
-            <h3>{task.text}{' '} 
-                <FaTimes style={{color:'red', cursor:'pointer'}}
-                onClick={()=>onDelete(task.id)}/>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>onToggle(task.id)}>
+            <h3>{task.text} {' '}
+                <div>
+                <FaEdit style={{color:'grey',cursor:'pointer'}}
+                    onClick={() => setShowEditTask(!showEditTask)} showEdit={showEditTask} />
+                <FaTimes style={{color:'red',cursor:'pointer'}} 
+                    onClick={ () => onDelete(task.id)}/>
+                </div>
             </h3>
             <p>{task.day}</p>
+            <div> {showEditTask&&<EditTask task={task} />}</div>
         </div>
     )
 }
