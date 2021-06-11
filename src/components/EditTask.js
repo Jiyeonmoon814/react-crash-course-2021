772a07ebf6c075a0ccfaab86bb493b7d7aa55b91
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-11 17:40:50 +0900

    update Task&EditTask js

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,40 +1,48 @@
 import { useState } from 'react'
 
+
 const EditTask = ({task, onEdit}) => {
-    const [text, setText] = useState('')
-    const [day, setDay] = useState('')
-    const [reminder, setReminder] = useState(false)
-    
+    const [id] = useState(task.id)
+    const [text, setText] = useState(task.text)
+    const [day, setDay] = useState(task.day)
+    const [reminder, setReminder] = useState(task.reminder)
+    const [hidden, setHidden] = useState(!task.hidden)
+
     const onSubmit = (e) => {
         e.preventDefault()
+        console.log(reminder)
         if(!text){
-            alert('Please add a task')
+            alert('Please fill a task')
             return
         }
-
-        onEdit({text,day,reminder})
-
-        setText('')
-        setDay('')
-        setReminder(false)
+        
+        onEdit({id,text,day,reminder,hidden})
+        
+        setText(text)
+        setDay(day)
+        setReminder(reminder)
+        setHidden(hidden)
+        
     }
 
+    
+
     return (
-        <form className='add-form' onSubmit={onSubmit}>
+        <form className='add-form' onSubmit={onSubmit} >
             <div className='form-control'>
-                <label>Task</label>
+                <label>Update Task</label>
                 <input type='text' placeholder={task.text} value={text} onChange={(e)=>setText(e.target.value)} />
             </div>
             <div className='form-control'>
-                <label>Day & Time</label>
+                <label>Update Day & Time</label>
                 <input type='text' placeholder={task.day} value={day} onChange={(e)=>setDay(e.target.value)} />
             </div>
             <div className='form-control form-control-check'>
-                <label>Set Reminder</label>
+                <label>Update Reminder</label>
                 <input type='checkbox' checked={task.reminder} value={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)} />
             </div>
 
-            <input type='submit' value='Save Task' class='btn btn-block' />
+            <input type='submit' value='Update Task' class='btn btn-block' />
         </form>
     )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,23 +1,24 @@
 import { FaTimes } from 'react-icons/fa';
 import { FaEdit } from 'react-icons/fa';
-import { useState } from 'react'
-
+import { useState } from 'react';
 import EditTask from './EditTask';
 
-const Task = ({task , onDelete, onToggle}) => {
+const Task = ({task , onDelete, onToggle, onEdit}) => {
     const [showEditTask, setShowEditTask] = useState(false)
+    const [hidden, setHidden] = useState(task.hidden)
+
     return (
         <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>onToggle(task.id)}>
             <h3>{task.text} {' '}
                 <div>
                 <FaEdit style={{color:'grey',cursor:'pointer'}}
-                    onClick={() => setShowEditTask(!showEditTask)} showEdit={showEditTask} />
+                    onClick={(e) => setShowEditTask(!showEditTask)}  showAdd={showEditTask} />
                 <FaTimes style={{color:'red',cursor:'pointer'}} 
                     onClick={ () => onDelete(task.id)}/>
                 </div>
             </h3>
             <p>{task.day}</p>
-            <div> {showEditTask&&<EditTask task={task} />}</div>
+            <div> {showEditTask && <EditTask task={task} onEdit={onEdit} /> }</div>
         </div>
     )
 }
